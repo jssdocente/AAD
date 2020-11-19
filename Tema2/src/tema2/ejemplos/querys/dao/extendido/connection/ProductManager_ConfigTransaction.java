08749c919b248d52129ea6d8b745f4216f81fd44
jssdocente <jssgarcia.docente@gmail.com>
2020-11-19 10:21:46 +0100

    Tema2. DAO. Incluimos gestión de transacciones.

--- Tema2/src/tema2/ejemplos/querys/dao/extendido/connection/ProductManager_ConfigTransaction.java
+++ Tema2/src/tema2/ejemplos/querys/dao/extendido/connection/ProductManager_ConfigTransaction.java
@@ -1,20 +1,31 @@
 package tema2.ejemplos.querys.dao.extendido.connection;
 
-public class ProductManager_Config {
+import java.sql.Connection;
+
+public class ProductManager_ConfigTransaction {
 
     public static void main(String[] args) throws Exception {
 
         ProductDAO product = null;
+        Connection cnn = null;
 
-        //Leemos la configuracion, para ver qué Implementación cargar
+                //Leemos la configuracion, para ver qué Implementación cargar
         String dbuse = ProductManagerConfig.readConfig("dao_db");
 
-        if (dbuse.equalsIgnoreCase("mariadb"))
+        if (dbuse.equalsIgnoreCase("mariadb")) {
             product = new ProductDAOmdb();
-        else if (dbuse.equalsIgnoreCase("mysql"))
+            cnn = DBMariadbConnection.getConnection();
+        }
+        else if (dbuse.equalsIgnoreCase("mysql")) {
             product = new ProductDAOmsql();
-        else
+            cnn = DBMySqlConnection.getConnection();
+        }
+        else {
             throw new Exception("Implmentación de Producto no encontrada");
+        }
+
+
+        cnn.setAutoCommit(false);
 
         try {
             // agregar nuevo producto
@@ -24,12 +35,21 @@ public class ProductManager_Config {
             ProductDTO p = product.read(100);
             System.out.println(p);
 
+            if (1==1)
+                throw new Exception("La transacción se tiene que deshacer");
+
             // eliminar el producto con ID = 100
             product.delete(100);
 
+            cnn.commit();
+
         } catch (Exception ex) {
+            cnn.rollback();
             System.out.println("Errores en el procesamiento");
             ex.printStackTrace();
+
+        } finally {
+            cnn.setAutoCommit(true);
         }
     }
 
