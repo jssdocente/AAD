9329ed3c903a859e5083711cb5785a5bf3134861
jssdocente <jssgarcia.docente@gmail.com>
2021-01-21 09:38:56 +0100

    [ADD] CURSO ARQUITECTURA-SOLIDA. MODULO-REPOSITORIO 2.1/N
    Incluido 2º modulo parcial. Concepto repositorios general.

---
+++ Tema3/Curso/Arq-solida/2.repositorio/src/test/java/com/iesvi/test/shared/domain/builder/NoticiaBuilder.java
@@ -0,0 +1,42 @@
+package com.iesvi.test.shared.domain.builder;
+
+import com.iesvi.bo.Noticia;
+import com.sun.tools.corba.se.idl.constExpr.Not;
+import io.beanmother.core.ObjectMother;
+
+import java.util.Date;
+
+public class NoticiaBuilder {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private Date fecha;
+
+    public NoticiaBuilder withTitle(String title) {
+        this.titulo = title;
+        return this;
+    }
+
+    public NoticiaBuilder withAutor(String author) {
+        this.autor = author;
+        return this;
+    }
+
+    public NoticiaBuilder withDate(Date date) {
+        this.fecha = date;
+        return this;
+    }
+
+    public Noticia build() {
+        ObjectMother om = ObjectMother.getInstance();
+        Noticia nmother= om.bear("noticia",Noticia.class);
+
+        return new Noticia(
+                id!=0 ? id : nmother.getId(),
+                titulo!=null ? titulo : nmother.getTitulo(),
+                autor!=null ? autor : nmother.getTitulo(),
+                fecha!=null ? fecha : nmother.getFecha()
+        );
+    }
+}
